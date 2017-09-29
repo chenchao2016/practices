1cd63d19b00591ff0c6f08e3f3345e5bb092cb8c
chenchao <chaochen2016@sina.com>
2017-09-29 23:15:59 +0800

    webpack

---
+++ webpack/b.js
@@ -0,0 +1,3 @@
+const index = require('./index.js');
+
+document.getElementById('root').appendChild(index());
---
+++ webpack/index.js
@@ -0,0 +1,6 @@
+module.exports = function(){
+    var greet = document.createElement('h1');
+    greet.textContent = 'hello world!';
+
+    return greet;
+}
